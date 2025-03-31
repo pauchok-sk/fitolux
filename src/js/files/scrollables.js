8a02f4bcce8a1020073eacc29255becf93546317
pauchok-sk <pauchoooooook@gmail.com>
2025-03-31 11:56:26 +0300

    decisions page

--- src/js/files/scrollables.js
+++ src/js/files/scrollables.js
@@ -2,4 +2,8 @@ import Scrollable from "../lib/Scrollable.js";
 
 export default function scrollables() {
   new Scrollable(".s-products__nav");
+
+  if (window.matchMedia("(max-width: 1365px)").matches) {
+    new Scrollable("#archive-nav")
+  }
 }
